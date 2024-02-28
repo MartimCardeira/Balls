53d3e207400cda556bd0382070a7661851923d4c
Martim de Assis Lopes Cardeira <martimalc1994@gmail.com>
2024-02-28 14:54:45 +0100

    ballsballsballs

---
+++ src/main/java/player.java
@@ -0,0 +1,26 @@
+import tudelft.wis.idm_tasks.boardGameTracker.interfaces.BoardGame;
+import tudelft.wis.idm_tasks.boardGameTracker.interfaces.Player;
+
+import java.util.Collection;
+
+public class player implements Player {
+    @Override
+    public String getPlayerName() {
+        return null;
+    }
+
+    @Override
+    public String getPlayerNickName() {
+        return null;
+    }
+
+    @Override
+    public Collection<BoardGame> getGameCollection() {
+        return null;
+    }
+
+    @Override
+    public String toVerboseString() {
+        return null;
+    }
+}
