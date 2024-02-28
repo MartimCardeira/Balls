334cec3a63ae80d9b3a8a141b5775ed4545f8cab
Martim de Assis Lopes Cardeira <martimalc1994@gmail.com>
2024-02-28 15:02:10 +0100

    ballsballsballs

--- src/main/java/PlayerJDBC.java
+++ src/main/java/PlayerJDBC.java
@@ -6,27 +6,27 @@ import java.util.Collection;
 public class PlayerJDBC implements Player {
     private String name;
     private String nickname;
-    private Collection<BoardGame> getGameCollection;
+    private Collection<BoardGame> gameCollection;
 
     public PlayerJDBC(String name, String nickname, Collection<BoardGame> getGameCollection) {
         this.name = name;
         this.nickname = nickname;
-        this.getGameCollection = getGameCollection;
+        this.gameCollection = getGameCollection;
     }
 
     @Override
     public String getPlayerName() {
-        return null;
+        return name;
     }
 
     @Override
     public String getPlayerNickName() {
-        return null;
+        return nickname;
     }
 
     @Override
     public Collection<BoardGame> getGameCollection() {
-        return null;
+        return gameCollection;
     }
 
     @Override
