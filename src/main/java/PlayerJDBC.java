d9ff02cd025b18bfa9bebeccbfa366593869659a
Martim de Assis Lopes Cardeira <martimalc1994@gmail.com>
2024-02-28 14:59:25 +0100

    ballsballsballs

--- src/main/java/PlayerJDBC.java
+++ src/main/java/PlayerJDBC.java
@@ -3,7 +3,10 @@ import tudelft.wis.idm_tasks.boardGameTracker.interfaces.Player;
 
 import java.util.Collection;
 
-public class player implements Player {
+public class PlayerJDBC implements Player {
+    private String name;
+    private String nickname;
+    private Collection<BoardGame> getGameCollection;
     @Override
     public String getPlayerName() {
         return null;
