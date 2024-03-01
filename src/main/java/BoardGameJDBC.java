2a51af8f4f2980fe3778ee277bf6c3073c8a48f5
Daniel Anschau <D.Anschau@student.tudelft.nl>
2024-03-01 18:51:58 +0100

    hashcode and equals for BoardgameJDBC

--- src/main/java/BoardGameJDBC.java
+++ src/main/java/BoardGameJDBC.java
@@ -1,5 +1,7 @@
 import tudelft.wis.idm_tasks.boardGameTracker.interfaces.BoardGame;
 
+import java.util.Objects;
+
 public class BoardGameJDBC implements BoardGame {
     private String name;
     private final String BGG_URL;
@@ -22,5 +24,18 @@ public class BoardGameJDBC implements BoardGame {
     @Override
     public String toVerboseString() {
         return name + " (" + BGG_URL + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BoardGameJDBC that = (BoardGameJDBC) o;
+        return Objects.equals(name, that.name) && Objects.equals(BGG_URL, that.BGG_URL);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, BGG_URL);
     }
 }
